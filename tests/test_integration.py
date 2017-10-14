8c3ae579deeac124636354b8a5024bc374b9cbf8
Chris White <white.cdw@gmail.com>
2017-10-14 10:44:06 -0400

    Finish test refactor...

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,41 +1,28 @@
-from unittest.mock import MagicMock
-
 from scully import Scully
 
 
-def test_scully_believes():
-    sc = MagicMock()
+def test_scully_believes(slack):
     msg = {'text': '<@U7G9A6Y7R>', 'channel': 'foo'}
-    bot = Scully(client=sc)
+    bot = Scully(client=slack)
     bot.slack_client.rtm_read.return_value = [msg]
     bot.listen()
-    args, kwargs = bot.slack_client.api_call.call_args
-    assert args == ('chat.postMessage',)
-    assert kwargs['text'] == 'I WANT TO BELIEVE'
-    assert kwargs['channel'] == 'foo'
+    assert bot.slack_client.api_called_with('chat.postMessage',
+                                            text='I WANT TO BELIEVE',
+                                            channel='foo')
 
 
-def test_scully_doesnt_believe():
-    sc = MagicMock()
+def test_scully_doesnt_believe(slack):
     msg = {'text': 'none', 'channel': 'foo'}
-    sc.rtm_read.return_value = [msg]
-    bot = Scully(client=sc)
+    slack.rtm_read.return_value = [msg]
+    bot = Scully(client=slack)
     bot.listen()
-    bot.slack_client.api_call.assert_not_called()
+    assert bot.slack_client.api_not_called()
 
 
-def test_scully_aliens():
-    sc = MagicMock()
+def test_scully_aliens(slack):
     msg = {'text': 'AlIeNs', 'channel': 'foo', 'ts': None}
-    bot = Scully(client=sc)
+    bot = Scully(client=slack)
     bot.slack_client.rtm_read.return_value = [msg]
     bot.listen()
-    call0, call1 = bot.slack_client.api_call.call_args_list
-    args0, kwargs0 = call0
-    args1, kwargs1 = call1
-    assert args0 == ('reactions.add',)
-    assert args1 == ('reactions.add',)
-    assert kwargs0['name'] == 'alien'
-    assert kwargs0['channel'] == 'foo'
-    assert kwargs1['name'] == 'telescope'
-    assert kwargs1['channel'] == 'foo'
+    assert bot.slack_client.api_called_with('reactions.add', name='alien', channel='foo')
+    assert bot.slack_client.api_called_with('reactions.add', name='telescope', channel='foo')
--- tests/test_responses.py
+++ tests/test_responses.py
@@ -4,28 +4,6 @@ from unittest.mock import MagicMock
 from scully.responses import AddReaction, AtMentions, Response
 
 
-@pytest.fixture
-def slack():
-    '''creates mock slack api with simpler testing API'''
-    class Slack(MagicMock):
-        def api_called_with(self, *args, **kwargs):
-            '''asserts at least one call was made with the provided args
-            and kwargs (kwargs may be a partial list)'''
-            for call in self.api_call.call_args_list:
-                agrees_with = []
-                a, k = call
-                agrees_with.append(args == a)
-                agrees_with.extend([k.get(key) == val for key, val in kwargs.items()])
-                if all(agrees_with):
-                    return True
-            return False
-
-        def api_not_called(self):
-            return not self.api_call.called
-
-    return Slack()
-
-
 def test_response_objects_reply_when_called():
     class TestResponse(Response):
         def reply(self, msg):
